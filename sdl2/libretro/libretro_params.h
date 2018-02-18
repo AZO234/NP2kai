efdfc8cae5b135045b9cf4c46b477260282d6d8b
Tomohiro Yoshidomi <sylph23k@gmail.com>
2018-02-18 23:33:01 +0900

    *.m3u load

--- sdl2/libretro/libretro_params.h
+++ sdl2/libretro/libretro_params.h
@@ -14,7 +14,7 @@
 
 #define LR_CORENAME        "Neko Project II kai"
 #define LR_LIBVERSION      NP2VER_CORE
-#define LR_VALIDFILEEXT    "d88|88d|d98|98d|fdi|xdf|hdm|dup|2hd|tfd|nfd|hd4|hd5|hd9|fdd|h01|hdb|ddb|dd6|dcp|dcu|flp|bin|fim|thd|nhd|hdi|vhd|sln|hdn"
+#define LR_VALIDFILEEXT    "d88|88d|d98|98d|fdi|xdf|hdm|dup|2hd|tfd|nfd|hd4|hd5|hd9|fdd|h01|hdb|ddb|dd6|dcp|dcu|flp|bin|fim|img|ima|thd|nhd|hdi|vhd|sln|hdn|m3u"
 #define LR_NEEDFILEPATH    true
 #define LR_BLOCKARCEXTRACT false
 #define LR_REQUIRESROM     false
