ae5dceb0d8cc32082c162e4cad90ce6f4f81c415
AZO234 <sylph23k@gmail.com>
2020-02-07 08:09:13 +0900

    fix int32_t

--- wab/cirrus_vga_extern.h
+++ wab/cirrus_vga_extern.h
@@ -98,7 +98,7 @@ typedef	signed char		int8_t;
 typedef	unsigned char	uint8_t;
 typedef	signed short	int16_t;
 typedef	unsigned short	uint16_t_;
-#if !defined(_3DS) && !defined(VITA)
+#if !defined(__LIBRETRO__)
 typedef	signed int		int32_t;
 #endif
 typedef	unsigned int	uint32_t_;
