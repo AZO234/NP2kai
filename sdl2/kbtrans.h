12c23166cc912b01c6db5905b51566ce60b80b01
AZO234 <sylph23k@gmail.com>
2020-05-03 13:06:59 +0900

    fix for emscripten build

--- sdl2/kbtrans.h
+++ sdl2/kbtrans.h
@@ -13,10 +13,12 @@ extern uint16_t keys_needed;
 extern LRKCNV*  keys_poll;
 
 void init_lrkey_to_pc98();
+void reset_lrkey();
 void send_libretro_key_down(UINT key);
 void send_libretro_key_up(UINT key);
 #else	/* __LIBRETRO__ */
 void sdlkbd_initialize(void);
+void sdlkbd_reset()
 void sdlkbd_keydown(UINT key);
 void sdlkbd_keyup(UINT key);
 #endif	/* __LIBRETRO__ */
