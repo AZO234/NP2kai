f9ab0cdd17cbbf6fae4f493b889227e415f19533
NeoClassic Tech <302284179+neoct@users.noreply.github.com>
2026-08-24 17:01:57 +0900

    pccore: clarify optional member initialization
    Use designated initializers for optional members so SUPPORT_DEBUGSS and SUPPORT_VIDEOFILTER changes do not shift subsequent fields.
    
    Unspecified members are initialized to zero by the C language rules.
    
    Signed-off-by: NeoClassic Tech <302284179+neoct@users.noreply.github.com>

--- pccore.c
+++ pccore.c
@@ -117,6 +117,11 @@ const OEMCHAR np2version[] = OEMTEXT(NP2KAI_GIT_TAG " " NP2KAI_GIT_HASH);
 #define PCBASEMULTIPLE 20
 #endif
 
+/*
+ * Use designated initializers for optional members so changes controlled by
+ * SUPPORT_DEBUGSS or SUPPORT_VIDEOFILTER do not shift subsequent fields.
+ * Unspecified members are initialized to zero by the C language rules.
+ */
 NP2CFG np2cfg = {
     0,
     1,
@@ -347,7 +352,7 @@ NP2CFG np2cfg = {
     OEMTEXT(""),
     0,
 #if defined(SUPPORT_DEBUGSS)
-    0,
+    .debugss = 0,
 #endif
 #if defined(SUPPORT_VIDEOFILTER)
     .vf1_enable = 0,
@@ -817,7 +822,7 @@ void pccore_setdefault(void) {
       OEMTEXT(""),
       0, 1,
 #if defined(SUPPORT_DEBUGSS)
-      0,
+      .debugss = 0,
 #endif
 #if defined(SUPPORT_VIDEOFILTER)
       .vf1_enable = 0,
